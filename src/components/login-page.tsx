26f3e7696c1fc2a8348cdd3d4a753c6eb4ae6747
HellBringer419 <35007817+HellBringer419@users.noreply.github.com>
2025-02-07 17:26:41 +0530

    some better css

--- src/components/login-page.tsx
+++ src/components/login-page.tsx
@@ -19,10 +19,16 @@ export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [showPassword, setShowPassword] = useState(false);
   const [emailError, _setEmailError] = useState("");
   const [passwordError, _setPasswordError] = useState("");
 
   const navigate = useNavigate();
+
+  const handleShowPassword = () => {
+    setShowPassword((current) => !current);
+  };
+
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     // Login logic here
@@ -38,7 +44,7 @@ export default function LoginPage() {
         </h2>
       </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md md:max-w-full">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md md:max-w-full lg:max-w-md">
         <Card>
           <CardHeader>
             <CardTitle>Welcome back</CardTitle>
@@ -62,6 +68,72 @@ export default function LoginPage() {
                 <small className="text-red-500">{emailError}</small>
               </div>
 
+              <div>
+                <Label htmlFor="password">Password</Label>
+                <div className="flex flex-row">
+                  <Input
+                    id="password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    minLength={8}
+                    maxLength={70}
+                    required
+                    className="mt-1"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <button
+                    type="button"
+                    data-hs-toggle-password='{ "target": "#hs-toggle-password" }'
+                    className="flex items-center z-20 mt-1 ml-1 px-3 cursor-pointer text-gray-400 rounded-e-md focus:outline-none focus:text-blue-600 dark:text-neutral-600 dark:focus:text-blue-500"
+                    onClick={handleShowPassword}
+                  >
+                    {/* SVG for password eye */}
+                    <svg
+                      className="shrink-0 size-3.5"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path
+                        className="hs-password-active:hidden"
+                        d="M9.88 9.88a3 3 0 1 0 4.24 4.24"
+                      ></path>
+                      <path
+                        className="hs-password-active:hidden"
+                        d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"
+                      ></path>
+                      <path
+                        className="hs-password-active:hidden"
+                        d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"
+                      ></path>
+                      <line
+                        className="hs-password-active:hidden"
+                        x1="2"
+                        x2="22"
+                        y1="2"
+                        y2="22"
+                      ></line>
+                      <path
+                        className="hidden hs-password-active:block"
+                        d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"
+                      ></path>
+                      <circle
+                        className="hidden hs-password-active:block"
+                        cx="12"
+                        cy="12"
+                        r="3"
+                      ></circle>
+                    </svg>
+                    {/* END OF SVG for password eye */}
+                  </button>
+                </div>
+                <small className="text-red-500">{passwordError}</small>
+              </div>
               <div>
                 <Label htmlFor="password">Password</Label>
                 <Input
